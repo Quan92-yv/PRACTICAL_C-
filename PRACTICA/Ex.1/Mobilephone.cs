bf0b5e78b5c2fd45de4beeaf438fbd4fb39f41fe
Quan-92 <9602tranquan@gmail.com>
2021-06-10 10:59:47 +0700

    Ex1

---
+++ PRACTICA/Ex.1/Mobilephone.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Ex1
+{
+    public class MobilePhone : Phone
+    {
+      
+    }
+}
---
+++ PRACTICA/Ex.1/phone.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Ex1
+{
+    public class Phone
+    {
+        public string PhoneName { get; set; }
+        public string PhoneType { get; set; }
+        public float PhonePrice { get; set; }
+
+        public void display()
+        {
+            Console.WriteLine($"Phone {PhoneName}, type {PhoneType}, price {PhonePrice}");
+        }
+    }
+}
